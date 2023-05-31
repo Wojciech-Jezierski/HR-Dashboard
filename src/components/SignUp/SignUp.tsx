293513761fc729ccaee411a9e5fd8dc0d0e5005d
wojciech-jezierski <wojtek20000@poczta.onet.pl>
2023-06-01 01:27:16 +0200

    quick fix

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './SignUp';
+import './SignUp.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { string, InferType } from 'yup';
 import * as Yup from 'yup';
