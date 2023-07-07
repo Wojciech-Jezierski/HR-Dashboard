de8ad95d7c6725e34e4cff9ade118e3e04b7bc50
wojciech-jezierski <wojtek20000@poczta.onet.pl>
2023-07-07 23:52:13 +0200

    Update SingleJob component

--- src/components/SingleJob/SingleJob.tsx
+++ src/components/SingleJob/SingleJob.tsx
@@ -1,37 +1,25 @@
 import React from 'react';
-import { BiUser } from 'react-icons/bi';
-import { GrLogout } from 'react-icons/gr';
-import { useState, useEffect, useRef } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { AiFillCloseCircle } from 'react-icons/ai';
-
-import { useFetch } from 'custom_hooks/useFetch';
-
-import { routerPaths } from '../../config/router';
 
 export const SingleJob = () => {
-  const { profile } = routerPaths;
-
-  const [data, setData] = useState(null);
-  const [title, setTitle] = useState(null);
-  const [company, setCompany] = useState(null);
-  const [createdAt, setCreatedAt] = useState('');
-  const [longDescription, setLongDescription] = useState(null);
-  const [status, setStatus] = useState(null);
+  const [data, setData] = useState({
+    title: '',
+    companyName: '',
+    createdAt: '',
+    longDescription: '',
+    shortDescription: '',
+    status: '',
+  });
   const [fetchError, setFetchError] = useState(null);
-  const [isOpenAvatar, setIsOpenAvatar] = useState(false);
-  const { firstName, lastName } = useFetch();
 
-  document.title = `HR Dashboard - ${title}`;
-
-  const closeAvatarWindow = () => {
-    setIsOpenAvatar(false);
-  };
+  document.title = `HR Dashboard - ${data.title}`;
 
   const url = window.location.pathname;
-  const id = url.substring(url.lastIndexOf('/') + 2);
-  const token = localStorage.getItem('USER_TOKEN');
+  const id = url.substring(url.lastIndexOf('/') + 1);
+
+  const token =
+    localStorage.getItem('USER_TOKEN') || sessionStorage.getItem('USER_TOKEN');
   const auth = `Bearer ${token}`;
   useEffect(() => {
     const fetchData = async () => {
@@ -39,18 +27,21 @@ export const SingleJob = () => {
         const response = await axios.get(`http://localhost:9595/jobs/${id} `, {
           headers: { Authorization: auth },
         });
-        console.log(response.data);
-        setData(response.data);
-        setTitle(response.data.title);
-        setCompany(response.data.companyName);
-        setCreatedAt(response.data.createdAt);
-        setStatus(response.data.status);
-        setLongDescription(response.data.longDescription);
+        setData({
+          title: response.data.title,
+          companyName: response.data.companyName,
+          createdAt: response.data.createdAt,
+          shortDescription: response.data.shortDescription,
+          longDescription: response.data.longDescription,
+          status: response.data.status,
+        });
       } catch (error: any) {
         setFetchError(error.message);
       }
     };
-
+    if (!token) {
+      return;
+    }
     fetchData();
   }, []);
 
@@ -64,64 +55,16 @@ export const SingleJob = () => {
 
   return (
     <div className="profile">
-      {isOpenAvatar ? (
-        <div className="window-container">
-          <div className="window relative">
-            <button
-              className="absolute right-[-12px] top-[-10px] text-2xl"
-              onClick={closeAvatarWindow}
-            >
-              <AiFillCloseCircle />
-            </button>
-            <NavLink to="">
-              <div className="flex hover:bg-slate-200 rounded-lg p-5">
-                <p className="mt-2 mr-4 text-2xl bg-gray-200 w-10 h-8 rounded-xl">
-                  {firstName.charAt(0)}
-                  {lastName.charAt(0)}
-                </p>
-                <p className="text-2xl mt-2">
-                  {firstName} {lastName}
-                </p>
-              </div>
-            </NavLink>
-            <NavLink to={profile.url}>
-              <div className="flex text-2xl mt-8 hover:bg-slate-200 rounded-lg p-5">
-                <BiUser className="text-4xl mr-4" />
-                Profile
-              </div>
-            </NavLink>
-            <NavLink to="">
-              <div className="flex text-2xl mt-8 hover:bg-slate-200 rounded-lg p-5">
-                <GrLogout className="text-4xl mr-4" />
-                Logout
-              </div>
-            </NavLink>
-          </div>
-        </div>
-      ) : (
-        <div className="single-job-content">
-          <h1 className="text-4xl">Job: {title}</h1>
-          <p className="text-xl mt-10">Company: {company}</p>
-          <p className="text-xl mt-2">
-            Created at: {createdAt.substring(0, 10)}{' '}
-            {createdAt.substring(11, 16)}
-          </p>
-          <p className="text-xl mt-2">Status: {status}</p>
-          <p className="text-xl mt-2">Description: {longDescription}</p>
-        </div>
-      )}
-      <button
-        onClick={() => {
-          setIsOpenAvatar(!isOpenAvatar);
-        }}
-      >
-        <div className="avatar w-[50px] h-[50px] fixed top-5 right-10 rounded-full bg-gray-300 text-white">
-          <p className="text-center mt-2 text-2xl">
-            {firstName.charAt(0)}
-            {lastName.charAt(0)}
-          </p>
-        </div>
-      </button>
+      <div className="single-job-content">
+        <h1 className="text-4xl">Job: {data.title}</h1>
+        <p className="text-xl mt-10">Company: {data.companyName}</p>
+        <p className="text-xl mt-2">
+          Created at: {data.createdAt.substring(0, 10)}{' '}
+          {data.createdAt.substring(11, 16)}
+        </p>
+        <p className="text-xl mt-2">Status: {data.status}</p>
+        <p className="text-xl mt-2">Description: {data.longDescription}</p>
+      </div>
     </div>
   );
 };
