d40d1b5a170b3df452de7a4621f7f7218c204780
wojciech-jezierski <wojtek20000@poczta.onet.pl>
2023-07-10 23:54:30 +0200

    Update SignUp components styles

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Form.css';
+import './SignUp.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { InferType } from 'yup';
 import { useForm } from 'react-hook-form';
