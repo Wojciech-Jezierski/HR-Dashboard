f4aa7ae07ec37dd8e7d2d916f9dc46938a0ca06a
Wojciech <wjezierski23@gmail.com>
2023-07-29 02:06:01 +0100

    Update Jobs component

--- src/components/Jobs/Jobs.tsx
+++ src/components/Jobs/Jobs.tsx
@@ -5,11 +5,15 @@ import { NavLink } from 'react-router-dom';
 import { AiFillEye } from 'react-icons/ai';
 import { BiEditAlt } from 'react-icons/bi';
 import { MdDelete } from 'react-icons/md';
-import axios from 'axios';
 import ReactPaginate from 'react-paginate';
+import { ReactPaginateProps } from 'react-paginate';
 
-import { useAuthToken } from '../../custom_hooks/useAuthToken';
 import { Data } from '../../types/data';
+import {
+  getJobs,
+  deleteItem,
+  deleteSelectedItems as deleteItemsService,
+} from '../../services/JobService';
 
 export const Jobs = () => {
   document.title = `HR Dashboard - Jobs`;
@@ -17,27 +21,37 @@ export const Jobs = () => {
   const [data, setData] = useState<Data[]>([]);
   const [selectedItems, setSelectedItems] = useState<String[]>([]);
   const [selectedOption, setSelectedOption] = useState('Actions');
+  const [isFocused, setIsFocused] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [filteredData, setFilteredData] = useState<Data[]>([]);
+  const [alert, setAlert] = useState('');
+  const [alertColor, setAlertColor] = useState('');
 
   const isDisabled = data.filter((item) => item.select).length < 3;
 
-  const [currentItems, setCurrentItems] = useState<Object[]>([]);
+  const [currentItems, setCurrentItems] = useState<Data[]>([]);
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 6;
 
-  const token = useAuthToken();
+  const token =
+    localStorage.getItem('USER_TOKEN') || sessionStorage.getItem('USER_TOKEN');
   const auth = `Bearer ${token}`;
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get('http://localhost:9595/jobs', {
-        headers: { Authorization: auth },
-      });
-      setData(result.data);
+    const getData = async () => {
+      if (!token) {
+        return;
+      }
+
+      try {
+        const fetchedData = await getJobs(auth);
+        setData(fetchedData);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
     };
-    if (!token) {
-      return;
-    }
-    fetchData();
+
+    getData();
   }, [token]);
 
   useEffect(() => {
@@ -53,39 +67,49 @@ export const Jobs = () => {
     setSelectedItems(items);
   }, [data]);
 
-  const handlePageClick = (event: any) => {
-    const newOffset = (event.selected * itemsPerPage) % data.length;
+  const handlePageClick: ReactPaginateProps['onPageChange'] = ({
+    selected,
+  }) => {
+    const newOffset = (selected * itemsPerPage) % data.length;
     setItemOffset(newOffset);
   };
 
-  const handleAllChange = (e: any) => {
-    const { checked } = e.target;
-    const newData = data.map((item: any) => {
-      return {
-        ...item,
-        select: checked,
+  useEffect(() => {
+    if (alert !== '') {
+      const timer = setTimeout(() => {
+        setAlert('');
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
       };
-    });
+    }
+    return undefined;
+  }, [alert]);
+
+  const handleAllChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    const newData = data.map((item: Data) => ({
+      ...item,
+      select: checked,
+    }));
 
     setData(newData);
   };
 
   const handleDelete = async (itemId: String) => {
     try {
-      // Make a DELETE request to the API endpoint with the item ID
-      const response = await fetch(`http://localhost:9595/jobs/:${itemId}`, {
-        method: 'DELETE',
-        headers: { Authorization: auth },
-      });
+      const response = await deleteItem(itemId, auth);
 
-      if (response.ok) {
+      if (response.success) {
         // Delete the item from the local state
-        setData((prevData: any) =>
-          prevData.filter((item: any) => item.id !== itemId),
-        );
+        setData((prevData) => prevData.filter((item) => item.id !== itemId));
         console.log('Item deleted successfully!');
+        setAlert('Item deleted successfully.');
+        setAlertColor('text-green-500');
       } else {
-        console.error('Failed to delete item');
+        setAlertColor('text-red-500');
+        setAlert(`${response.error}`);
       }
     } catch (error) {
       console.error('Error occurred while deleting item:', error);
@@ -93,26 +117,31 @@ export const Jobs = () => {
   };
 
   const deleteSelectedItems = async () => {
-    const selectedItem = data
-      .filter((item) => item.select === true)
-      .map((item) => item.id);
     try {
-      await Promise.all(
-        selectedItem.map((id) =>
-          axios.delete(`http://localhost:9595/jobs/${id}`, {
-            headers: { Authorization: auth },
-          }),
-        ),
-      );
-
-      const updatedData = data.filter((item) => item.select === undefined);
+      const updatedData = await deleteItemsService(data, auth);
       setData(updatedData);
       setSelectedItems([]);
-      console.log('Multiple elements deleted successfully!');
+      setAlert('Items deleted successfully.');
+      setAlertColor('text-green-500');
       setSelectedOption('Actions');
     } catch (error) {
-      console.log('Error deleting elements:', error);
+      setAlert(`${error}`);
+      setAlertColor('text-red-500');
     }
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setInputValue(value);
+  };
+
+  const handleClear = () => {
+    setInputValue('');
   };
 
   if (!data) {
@@ -125,16 +154,44 @@ export const Jobs = () => {
 
   return (
     <div className="jobs">
-      <div className="content text-sm sm:text-md mt-40 md:mt-12 w-[340px] md:w-[620px] xl:w-[760px]">
-        <select
-          className="p-2 mb-7 text-md"
-          disabled={isDisabled}
-          onChange={deleteSelectedItems}
-          value={selectedOption}
-        >
-          <option value="Actions">Actions</option>
-          <option value="Delete">Delete</option>
-        </select>
+      <div className="flex justify-center">
+        <span className={alertColor}>{alert}</span>
+      </div>
+      <div className="content text-sm sm:text-md mt-1 md:mt-12 w-[340px] md:w-[620px] xl:w-[760px]">
+        <div className={`input-wrapper ${isFocused ? 'focused' : ''}`}>
+          <select
+            className="p-2 md:mb-7 text-xl"
+            disabled={isDisabled}
+            onChange={deleteSelectedItems}
+            value={selectedOption}
+          >
+            <option value="Actions">Actions</option>
+            <option value="Delete">Delete</option>
+          </select>
+          <input
+            className="bg-slate-200 ml-3 w-64 h-8 text-xl mb-5 md:mb-0"
+            type="text"
+            placeholder="Search Job"
+            onFocus={handleFocus}
+            value={inputValue}
+            onChange={handleChange}
+          />
+          {inputValue && (
+            <span
+              className="clear-icon"
+              onClick={handleClear}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter' || event.key === 'Space') {
+                  handleClear();
+                }
+              }}
+              role="button"
+              tabIndex={0}
+            >
+              x
+            </span>
+          )}
+        </div>
         <div
           key="position"
           className="grid grid-cols-3 md:gap-20 gap-10 border-b-2"
@@ -151,56 +208,70 @@ export const Jobs = () => {
           <p className="md:ml-40 ml-20 xl:ml-52">Date</p>
           <p className="text-right md:mr-5 mr-0">Action</p>
         </div>
-        {currentItems.map((item: any) => {
-          return (
-            <div key={item.id} className="grid grid-cols-5 mt-2 border-b-2">
-              <label htmlFor="position-title" className="col-span-3 flex">
-                <input
-                  type="checkbox"
-                  className="mr-2"
-                  checked={item.select || false}
-                  onChange={(event) => {
-                    const { checked } = event.target;
-                    setData(
-                      /* eslint-disable-next-line @typescript-eslint/no-shadow */
-                      data.map((data: any) => {
-                        /* eslint-disable @typescript-eslint/no-shadow */
-                        if (item.id === data.id) {
-                          return {
-                            ...item,
-                            select: checked,
-                          };
-                        }
-                        return data;
-                      }),
-                    );
-                  }}
-                />
-                <h4 className="flex items-center">{item.title}</h4>
-              </label>
-              <h4 className="mt-5 mr-5">{item.createdAt.substring(0, 10)}</h4>
-              <div className="mt-5 ml-5 md:ml-16 xl:ml-20 text-xl flex">
-                <NavLink to={`/jobs/${item.id}`} className="md:mr-2 xl:mr-3">
-                  <AiFillEye />
-                </NavLink>
-                <NavLink
-                  to={`/jobs/${item.id}/edit`}
-                  className="md:mr-2 xl:md:mr-3"
-                >
-                  <BiEditAlt />
-                </NavLink>
-                <button
-                  className="text-xl  mb-7"
-                  onClick={() => {
-                    handleDelete(item.id);
-                  }}
-                >
-                  <MdDelete />
-                </button>
+        {currentItems
+          .filter((item: Data) => {
+            if (inputValue.length >= 3) {
+              return item.title
+                .toLowerCase()
+                .includes(inputValue.toLowerCase());
+            }
+            return true; // Return true to include all items when inputValue has less than 3 characters
+          })
+          .map((item: Data) => {
+            return (
+              <div
+                key={item.id.toString()}
+                className="grid grid-cols-5 mt-2 border-b-2"
+              >
+                <label htmlFor="position-title" className="col-span-3 flex">
+                  <input
+                    type="checkbox"
+                    className="mr-2"
+                    checked={item.select || false}
+                    onChange={(event) => {
+                      const { checked } = event.target;
+                      setData(
+                        /* eslint-disable-next-line @typescript-eslint/no-shadow */
+                        data.map((data: Data) => {
+                          /* eslint-disable @typescript-eslint/no-shadow */
+                          if (item.id === data.id) {
+                            return {
+                              ...item,
+                              select: checked,
+                            };
+                          }
+                          return data;
+                        }),
+                      );
+                    }}
+                  />
+                  <h4 className="flex items-center">{item.title}</h4>
+                </label>
+                <h4 className="mt-5 mr-5">
+                  {String(item.createdAt).substring(0, 10)}
+                </h4>
+                <div className="mt-5 ml-5 md:ml-16 xl:ml-20 text-xl flex">
+                  <NavLink to={`/jobs/${item.id}`} className="md:mr-2 xl:mr-3">
+                    <AiFillEye />
+                  </NavLink>
+                  <NavLink
+                    to={`/jobs/${item.id}/edit`}
+                    className="md:mr-2 xl:md:mr-3"
+                  >
+                    <BiEditAlt />
+                  </NavLink>
+                  <button
+                    className="text-xl  mb-7"
+                    onClick={() => {
+                      handleDelete(item.id);
+                    }}
+                  >
+                    <MdDelete />
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
         <ReactPaginate
           breakLabel="..."
           nextLabel=">"
