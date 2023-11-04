8dcb5c2841ba70d4df8fb73d3b65ccbf049a0b60
Wojciech-Jezierski <wojtek20000@poczta.onet.pl>
2023-11-04 17:21:39 +0100

    Change output build folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './dist/hr-dashboard'
+    outDir: './build'
   },
   server: {
     port: 4200,
